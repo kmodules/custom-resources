9fe8aeeac805873a4678286b2a0e4772f85a6a6a
Tamal Saha <tamal@appscode.com>
2018-12-19 01:33:36 -0800

    Copy TransformCredentials from service-catalog
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- vendor/k8s.io/client-go/util/jsonpath/doc.go
+++ vendor/k8s.io/client-go/util/jsonpath/doc.go
@@ -1,4 +1,5 @@
-Copyright 2011-2016 Canonical Ltd.
+/*
+Copyright 2015 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -11,3 +12,9 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+*/
+
+// package jsonpath is a template engine using jsonpath syntax,
+// which can be seen at http://goessner.net/articles/JsonPath/.
+// In addition, it has {range} {end} function to iterate list and slice.
+package jsonpath // import "k8s.io/client-go/util/jsonpath"
