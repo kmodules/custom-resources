ed1c15a0bbda8ae9e4a75d293b0001e1039bacba
Tamal Saha <tamal@appscode.com>
2019-02-24 17:20:57 -0800

    Update Kubernetes client libraries to 1.13.0 (#17)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -17,7 +17,7 @@ mkdir -p "$REPO_ROOT"/api/api-rules
 docker run --rm -ti -u $(id -u):$(id -g) \
   -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
   -w "$DOCKER_REPO_ROOT" \
-  appscode/gengo:release-1.12 "$DOCKER_CODEGEN_PKG"/generate-groups.sh all \
+  appscode/gengo:release-1.13 "$DOCKER_CODEGEN_PKG"/generate-groups.sh all \
   kmodules.xyz/custom-resources/client \
   kmodules.xyz/custom-resources/apis \
   "appcatalog:v1alpha1" \
@@ -28,7 +28,7 @@ for gv in "${apiGroups[@]}"; do
   docker run --rm -ti -u $(id -u):$(id -g) \
     -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
     -w "$DOCKER_REPO_ROOT" \
-    appscode/gengo:release-1.12 openapi-gen \
+    appscode/gengo:release-1.13 openapi-gen \
     --v 1 --logtostderr \
     --go-header-file "hack/gengo/boilerplate.go.txt" \
     --input-dirs "$PACKAGE_NAME/apis/${gv},k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/util/intstr,k8s.io/apimachinery/pkg/version,k8s.io/api/core/v1,k8s.io/api/apps/v1" \
